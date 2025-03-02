c4728d456300fec92d0fcef1c20034ed93f512f8
Arun Kumar Venkatesan <97891813+arunjavadxc@users.noreply.github.com>
2025-03-02 21:29:32 +0530

    UI Layout changes

--- UI/src/app/components/views/home/home.component.ts
+++ UI/src/app/components/views/home/home.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../../shared/navbar/navbar.component";
+import { SidebarComponent } from "../../shared/sidebar/sidebar.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  isSidebarOpen: boolean = true;
+
+  onSidebarToggle() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
 
 }
