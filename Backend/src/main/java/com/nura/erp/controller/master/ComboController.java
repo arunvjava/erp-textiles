ee262f2b665524bb889fbb0ba104a47ed1675e6d
Arun Kumar Venkatesan <97891813+arunjavadxc@users.noreply.github.com>
2025-04-14 23:26:03 +0530

    Combo apis completed

--- Backend/src/main/java/com/nura/erp/controller/master/ComboController.java
+++ Backend/src/main/java/com/nura/erp/controller/master/ComboController.java
@@ -1,5 +1,7 @@
 package com.nura.erp.controller.master;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.PageRequest;
@@ -9,14 +11,16 @@ import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.nura.erp.entity.master.Brand;
 import com.nura.erp.entity.master.Combo;
 import com.nura.erp.service.master.ComboService;
 import com.nura.erp.utils.Constants;
@@ -52,6 +56,19 @@ public class ComboController {
 	public ResponseEntity<Response<Combo>> saveBrand(@Valid @RequestBody Combo combo) {
 		Combo savedCombo = comboService.saveCombo(combo);
 		return ResponseEntity.ok(Response.<Combo>builder().status(201).respObj(savedCombo).build());
+	}
+	
+	@PutMapping
+	public ResponseEntity<Response<Combo>> updateCombo(@Valid @RequestBody Combo combo) {
+		Combo savedCombo = comboService.saveCombo(combo);
+		return ResponseEntity.ok(Response.<Combo>builder().status(200).respObj(savedCombo).build());
+	}
+	
+	@DeleteMapping("/{ids}")
+	public ResponseEntity<Response<String>> deletecolors(@PathVariable String ids) {
+		List<Long> idList = Arrays.stream(ids.split(",")).map(Long::parseLong).toList();
+		comboService.deleteComboByID(idList);
+		return ResponseEntity.ok(Response.<String>builder().status(200).respObj(null).build());
 	}
 
 }
